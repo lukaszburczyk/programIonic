fb4c33ac250ebe0dc208c31f15cbf42932bad0bd
Łukasz Burczyk <lukasz.burczyk@outlook.com>
2016-12-28 15:16:56 +0100

    Program testowy angularjs v.1 ionic v.1

---
+++ programIonic/platforms/android/assets/www/js/app.js
@@ -0,0 +1,78 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html'
+    })
+
+          .state('app.index', {
+              url: '/index',
+              views: {
+                  'menuContent': {
+                      templateUrl: 'templates/index.html'
+                  }
+              }
+          })
+          .state('app.email', {
+              url: '/email',
+              views: {
+                  'menuContent': {
+                      templateUrl: 'templates/email.html'
+                  }
+              }
+          })
+          .state('app.listaGit', {
+              url: '/listaGit',
+              views: {
+                  'menuContent': {
+                      templateUrl: 'templates/listaGit.html',
+                      controller: 'listaGit'
+                  }
+              }
+          })
+                  .state('app.listaGitSz', {
+                      url: '/listaGit/:name',
+                      views: {
+                          'menuContent': {
+                              templateUrl: 'templates/listaGitSz.html',
+                              controller: 'szczeP'
+                          }
+                      }
+                  })
+  .state('app.lista', {
+      url: '/lista',
+      views: {
+          'menuContent': {
+              templateUrl: 'templates/lista.html'
+          }
+      }
+  });
+    $urlRouterProvider.otherwise('/app/index');
+})
---
+++ programIonic/platforms/ios/www/js/controllers.js
@@ -0,0 +1,44 @@
+﻿angular.module('starter.controllers', [])
+.controller("wiadomosc", function ($scope) {
+    $scope.witaj = "Witaj w aplikacji! Wybierz przycisk!";
+
+})
+
+.controller("lokalizacja", function ($scope, $location) {
+    $scope.go = function (path) {
+        $location.path(path);
+    };
+})
+
+.controller("lista", function ($scope) {
+    $scope.elementy = [
+        { name: "jeden" },
+        { name: "dwa" },
+        { name: "trzy" },
+        { name: "cztery" }
+    ];
+})
+.controller("listaGit", function ($scope, $http) {
+    $scope.username = "Microsoft";        
+
+    var odpowiedzSerwer = function (response) {
+        $scope.user = response.data;
+
+        $http.get($scope.user.repos_url).
+        then(onRepos, onError);
+    }
+
+    var onRepos = function (response) {
+        $scope.repos = response.data;
+    }
+
+    var onError = function (reason) {
+        $scope.error = "Nie można pobrać informacji"
+    }
+
+    $scope.search = function (username) {
+        $http.get("https://api.github.com/users/" + username).
+        then(odpowiedzSerwer, onError);
+
+    }
+})
