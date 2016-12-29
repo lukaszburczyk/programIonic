0b884755c9bcc68d2e6b33d5447719c8d6144759
Łukasz Burczyk <lukasz.burczyk@outlook.com>
2016-12-29 15:58:32 +0100

    listaDane

---
+++ listaDane/platforms/android/assets/www/js/app.js
@@ -0,0 +1,63 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html'
+    })
+
+          .state('app.lista1', {
+              url: '/lista1',
+              views: {
+                  'lista1': {
+                      templateUrl: 'templates/lista1.html',
+                      controller: 'lista'
+                  }
+              }
+          })
+    .state('app.lista2', {
+        url: '/lista2',
+        views: {
+            'lista2': {
+                templateUrl: 'templates/lista2.html',
+                controller: 'lista'
+            }
+        }
+    })
+    .state('app.lista3', {
+        url: '/lista3',
+        views: {
+            'lista3': {
+                templateUrl: 'templates/lista3.html',
+                controller: 'lista'
+            }
+        }
+    });
+    $urlRouterProvider.otherwise('/app/lista1');
+})
---
+++ listaDane/platforms/ios/www/js/controllers.js
@@ -0,0 +1,90 @@
+﻿angular.module('starter.controllers', [])
+
+.controller("lista", function ($scope, $http, $ionicPopup, $location) {
+    $scope.url = $location.path();
+    $scope.url2 = $scope.url.replace("/app/lista", "");
+
+    $scope.start = function () {
+        $http.get("daneJSON"+$scope.url2+".JSON").
+                then(odpowiedzSerwer, onError);
+    }
+
+    var odpowiedzSerwer = function (response) {
+        $scope.user = response.data;
+    }
+
+    var onError = function (reason) {
+        $scope.error = "Nie można pobrać informacji"
+    }
+
+    $scope.showPopup = function (user) {
+        $scope.myPopup = $ionicPopup.show({
+            template:
+     '<div class="list">'
+    +   '<a class="item item-icon-left">'
+    +       '<i class="icon ion-ios-home"></i>'
+    +       user.jednostka
+    +   '</a>'
+    +   '<a class="item item-icon-left">'
+    +       '<i class="icon ion-person"></i>'
+    +       user.rola
+    +   '</a>'
+    + '<a class="item item-icon-left" href="mailto:'+user.email+'">'
+    +       '<i class="icon ion-email"></i>'
+    +       user.email
+    +   '</a>'
+    + '<a class="item item-icon-left item-icon-right" href="tel:'+user.mobile+'">'
+    +       '<i class="icon ion-chatbubble-working"></i>'
+    +       user.mobile
+    +       '<i class="icon ion-ios-telephone-outline"></i>'
+    +   '</a>'
+    + '</div>',
+            scope: $scope,
+            buttons: [
+                { text: 'Powrót' }
+            ]
+        })
+    }
+
+    $scope.stworzyl = function () {
+        $scope.myPopup = $ionicPopup.show({
+            template:
+     '<div class="list">'
+    + '<a class="item item-icon-left">'
+    + '<i class="icon ion-social-angular"></i>'
+    + 'Łukasz Burczyk'
+    + '</a>'
+    + '<a class="item item-icon-left">'
+    + '<i class="icon ion-ios-home"></i>'
+    + 'Orange/IT'
+    + '</a>'
+    + '<a class="item item-icon-left">'
+    + '<i class="icon ion-person"></i>'
+    + 'Programista Aplikacji Mobilnych'
+    + '</a>'
+    + '<a class="item item-icon-left" href="mailto:lukasz.burczyk@outlook.com">'
+    + '<i class="icon ion-email"></i>'
+    + 'lukasz.burczyk@outlook.com'
+    + '</a>'
+    + '<a class="item item-icon-left item-icon-right" href="tel:+48 791360579">'
+    + '<i class="icon ion-chatbubble-working"></i>'
+    + '+48 791360579'
+    + '<i class="icon ion-ios-telephone-outline"></i>'
+    + '</a>'
+    + '</div>',
+            scope: $scope,
+            buttons: [
+                { text: 'Powrót' }
+            ]
+        })
+    }
+
+    $scope.zamknijPopup = function () {
+        $scope.myPopup.close();
+    }
+
+    $scope.isEmpty = function (obj) {
+        for (var i in obj) if (obj.hasOwnProperty(i)) return true;
+        return false;
+    }
+});
